6cd96a728a528212dff9cabe65a0f04c3516e707
Varun Harinath <varunharinath15@gmail.com>
2024-09-11 13:56:53 +0530

    Update to Make Production ready

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import ShiftLogRouter from "./routes/ShifLogFormRoutes.js";
 import GenerateSummaryRouter from "./routes/GenerateSummaryRoute.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import cron from "node-cron";
+import fetch from "node-fetch"; // Install node-fetch if you don't have it
+
 dotenv.config();
 
 const app = express();
@@ -19,6 +22,16 @@ app.get("/", (req, res) => {
 app.use("/api", ShiftLogRouter);
 app.use("/", GenerateSummaryRouter);
 
+// Cron job to ping the server every 10 minutes
+cron.schedule("*/10 * * * *", async () => {
+  try {
+    await fetch(process.env.CALLBACK_URL);
+    console.log("Pinged  successfully");
+  } catch (error) {
+    console.error("Error pinging ", error);
+  }
+});
+
 app.listen(process.env.PORT, () =>
-  console.log(`Server is running on port:${process.env.PORT}`)
+  console.log(`Server is running on port: ${process.env.PORT}`)
 );
